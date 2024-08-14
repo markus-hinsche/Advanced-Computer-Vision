a0d18f33290ee535d77f142de084c0f5dfd2a2fe
Markus Hinsche <m.hinsche@gmail.com>
2024-08-14 21:01:33 +0200

    Fix Module 1

--- Module 1 - GoogLeNet Project/tests.py
+++ Module 1 - GoogLeNet Project/tests.py
@@ -10,25 +10,25 @@ def colored_string(s, color='black'):
 
 def TEST_INCEPTIONBLOCK(InceptionBlock):
 
-    if InceptionBlock(Input(shape = (224, 224, 3)), 10, (10, 10), (10, 10), 10).shape[1:] == [224, 224, 40]:
+    if list(InceptionBlock(Input(shape = (224, 224, 3)), 10, (10, 10), (10, 10), 10).shape[1:]) == [224, 224, 40]:
         return html_print(colored_string("IMPLEMENTATION IS CORRECT! GOOD JOB!", 'green'))
     else:
         return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT", "red"))
 
 
-    
+
 def TEST_AUXILARY(AuxilaryClassifier):
     inputs = Input(shape = (10, 10, 1))
     aux = AuxilaryClassifier(inputs)
     model = Model(inputs=inputs, outputs=aux)
     layers = model.layers
-    
+
     aux_test = True
 
     if not model.count_params() == 526593:
         aux_test = False
         return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT", "red"))
-        
+
     if not type(layers[1]) == AveragePooling2D:
         aux_test = False
         return html_print(colored_string("FIRST LAYER SHOULD BE AveragePooling2D", "red"))
@@ -39,7 +39,7 @@ def TEST_AUXILARY(AuxilaryClassifier):
 
     if not type(layers[3]) == Flatten:
         aux_test = False
-        return html_print(colored_string("THIRD LAYER SHOULD BE Flatten", "red"))   
+        return html_print(colored_string("THIRD LAYER SHOULD BE Flatten", "red"))
 
     if not type(layers[4]) == Dense:
         aux_test = False
@@ -61,7 +61,7 @@ def TEST_GOOGLENET(model):
 
     if not model.count_params() == 8471347:
         return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT, CHECK YOUR LAYERS AGAIN", "red"))
-        
+
     if not len(model.outputs) == 3:
         return html_print(colored_string("MAKE SURE THAT YOUR OUTPUTS HAVE 3 PARTS. 2 AUXILARY CLASSIFIERS AND FINAL OUTPUT", "red"))
 
