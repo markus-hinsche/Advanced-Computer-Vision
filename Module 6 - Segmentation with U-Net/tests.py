825a4d8c835c2b60ccc3b8ed535675c24b68b5d4
Markus Hinsche <m.hinsche@gmail.com>
2024-11-01 09:10:02 +0100

    Fix tests

--- Module 6 - Segmentation with U-Net/tests.py
+++ Module 6 - Segmentation with U-Net/tests.py
@@ -10,35 +10,30 @@ def colored_string(s, color='black'):
 def TEST_ENCODER_BLOCK(encoder_block):
 
     outs = encoder_block(Input(shape=(224, 224, 3)), 32)
-    
+
     if len(list(outs)) != 2:
         return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. THIS FUNCTION SHOULD RETURN TWO LAYERS", "red"))
 
-    if "max_pooling2d" not in outs[0].name :
-        return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. FIRST OUTPUT SHOULD BE FROM THE MaxPooling2d LAYER", "red"))
+    if (112, 112, 32) != outs[0].shape[1:]:
+        return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. FIRST OUTPUT SHOULD BE FROM THE MaxPooling2d LAYER with the correct shape", "red"))
 
+    if (224, 224, 32) != outs[1].shape[1:]:
+        return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. SECOND OUTPUT SHOULD BE FROM THE Conv2d LAYER with the correct shape", "red"))
 
-    if "conv2d" not in outs[1].name :
-        return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. SECOND OUTPUT SHOULD BE FROM THE Conv2d LAYER", "red"))
-    
-
-    if "max_pooling2d" not in outs[0].name :
-        return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. FIRST OUTPUT SHOULD BE FROM THE MaxPooling2d LAYER", "red"))
-    else:
-        return html_print(colored_string("IMPLEMENTATION IS CORRECT! GOOD JOB!", 'green'))
+    return html_print(colored_string("IMPLEMENTATION IS CORRECT! GOOD JOB!", 'green'))
 
 
 
 def TEST_DECODER_BLOCK(decoder_block, encoder_block):
-    
+
     outs = encoder_block(Input(shape=(224, 224, 3)), 32)
 
     dec = decoder_block(Input(shape=(112, 112, 3)), outs[1], 32)
-    
-    if "conv2d" not in dec.name :
-        return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. SECOND OUTPUT SHOULD BE FROM THE Conv2d LAYER", "red"))
-    
-    
+
+    # if "conv2d" not in dec.name :
+    #     return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. SECOND OUTPUT SHOULD BE FROM THE Conv2d LAYER", "red"))
+
+
     if list(dec.shape) !=  [None, 224, 224, 32]:
         return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT. CHECK LAYERS AGAIN AND MAKE SURE TO USE CONCAT", "red"))
     else:
@@ -46,7 +41,7 @@ def TEST_DECODER_BLOCK(decoder_block, encoder_block):
 
 
 def TEST_UNET_MODEL(model):
-    
+
 
     if model.count_params() != 1940817:
         return html_print(colored_string("IMPLEMENTATION IS NOT CORRECT.", "red"))
